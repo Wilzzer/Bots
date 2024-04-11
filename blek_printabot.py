e9d0437ef977d34df4ddb7a5f24811578b81c471
TuT <aremondeau@gmail.com>
2024-04-11 16:20:02 +0200

    INIT_COMIT

---
+++ blek_printabot.py
@@ -0,0 +1,116 @@
+import requests
+import re
+import json
+from fpdf import FPDF
+from PIL import Image
+
+AUTH_URL = "https://auth.blek.ch/"
+USER_FILE = "user.txt"
+TEST_FILE = "prout.pdf"
+
+auth_headers = {'Host': 'auth.blek.ch',
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+           'Accept-Language': 'en-US,en;q=0.5',
+           'Accept-Encoding': 'gzip, deflate, br',
+           'Content-Type': 'application/x-www-form-urlencoded',
+           'Origin': 'https://auth.blek.ch',
+           'Connection': 'keep-alive',
+           'Referer': 'https://auth.blek.ch/?cancel=1',
+           'Cookie': 'lemonldap=0; llngconnection=0',
+           'Upgrade-Insecure-Requests': '1',
+           'Sec-Fetch-Dest': 'document',
+           'Sec-Fetch-Mode': 'navigate',
+           'Sec-Fetch-Site': 'same-origin',
+           'Sec-Fetch-User': '?1'}
+
+def print_ordered_headers(headers):
+    mkeys = list(headers.keys())
+    mkeys.sort()
+    sorted = {i: headers[i] for i in mkeys}
+    print(sorted)
+
+def find_token(lines):
+    for line in lines:
+        if "token" in line:
+            print(line)
+            break
+    line = line[re.search("value", line).end():]
+    apo = []
+    [apo.append(m) for m in re.finditer('"', line)]
+    token = line[apo[0].end():apo[1].start()]
+    print(token)
+    return token
+
+def open_session(user_auth_data):
+    session = requests.Session()
+    response = session.get(AUTH_URL)
+
+    token = find_token(response.text.splitlines())
+    auth_data = "url&timezone=2&lmAuth=1_Balelec&skin=blek&token={}&user={}&password={}".format(token, user_auth_data['user'], user_auth_data['pw'])
+
+    response = session.post(AUTH_URL, headers=auth_headers, data=auth_data)
+    error = False
+    error_str = None
+    error_count = 0
+    for line in response.text.splitlines():
+        if (error):
+            error_str = error_str+"\n"+line
+            error_count = error_count+1
+        if "errormsg" in line:
+            error_str = line
+            error = True
+            error_count = error_count+1
+        if(error_count == 7):
+            break
+    return session, error_str
+
+def print_file(session, user_auth_data, filename):
+    print_file = filename
+
+    print_headers = {
+        'Accept': 'application/json'
+    }
+
+    files = {
+        'file': open(print_file, 'rb')
+    }
+
+    response = session.post(
+        url='https://print.blek.ch/restful/v1/documents/print',
+        headers=print_headers,
+        files = files,
+        # verify = False,
+        auth=(user_auth_data['user'], user_auth_data['UUID'])
+    )
+    print(response)
+
+def generate_pdf(file):
+    img = Image.open(file)
+    
+    if(img.width>img.height):
+        pdf_orient = "L"
+    else:
+        pdf_orient = "P"
+    pdf_orient = "L"
+    pdf = FPDF(orientation=pdf_orient, unit="mm", format="A4")
+    pdf.add_page()
+    # FPDF.eph
+    print(pdf.eph, pdf.epw, img.height, img.width)
+
+def main():
+    with open(USER_FILE) as f:
+        user_auth_data = json.load(f)
+    session, error = open_session(user_auth_data)
+    filename = TEST_FILE
+
+    generate_pdf("pinard.jpg")
+    if(not error):
+        response = session.get("https://print.blek.ch/restful/v1/tests/http/request")
+        print(response.text)
+        # print_file(session, user_auth_data, filename) 
+    else:
+        print("Found error in request blablabla :\n", error)
+
+if __name__ == '__main__':
+    main()
---
+++ zuliabot.py
@@ -0,0 +1,125 @@
+import logging
+import string
+import time
+from unidecode import unidecode
+from telegram import Update
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level = logging.INFO
+)
+
+ZUL = ["o",
+       "eau",
+       "au",
+       "aux",
+       "oh",
+       "ot",
+       "ots",
+       "ow",
+       "aud",
+       "aut",
+       "op",
+       "os"]
+
+CKU = ["seku",
+       "secu",
+       "ceku",
+       "cecu"]
+
+APAGN = ["apagn",
+         "gnan"]
+
+EXCEPT = ["miros",
+          "os",
+          "bot",
+          "hotspot",
+          "zuliabot",
+          "nullos",
+          "aux",
+          "au",
+          "matos",
+          "doritos",
+          "faut",
+          "bios",
+          "ddos",
+          "qos",
+          "albatros",
+          "albinos",
+          "craignos",
+          "asclepios",
+          "tetanos",
+          "calvados",
+          "galapagos",
+          "intramuros",
+          "gratos",
+          "thermos",
+          "pratos",
+          "rapidos",
+          "tekos",
+          "hop"]
+
+MACRON = ["macron",
+          "micron"]
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    translator = str.maketrans('','',string.punctuation)
+    words = update.message.text.lower().translate(translator)
+    words = unidecode(words).split()    
+    print(words)
+    for word in words:
+        for o in ZUL:
+            if word.endswith(o) and word not in EXCEPT:
+                zuliatext = "🤌🤌 "+word.upper()+" ZULIANI 🤌🤌"
+                await context.bot.send_message(chat_id=update.effective_chat.id, text=zuliatext)
+                time.sleep(0.1)
+                break
+        if word in APAGN:
+            await context.bot.send_animation(chat_id=update.effective_chat.id, animation="CgACAgQAAxkBAANEZgKWAAGeKiZ8tN_8mlRYZXmFAsxOAAJWBAACSYsdUUzZMU2eWmqVNAQ", reply_to_message_id=update.message.id)
+            return
+            
+        if(word in CKU):
+            await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker="CAACAgQAAxkBAANWZgKkKTBWXw4QXdnnzj4moP-PuYEAApQJAAIO6MlQCDZWevZwWR40BA", reply_to_message_id=update.message.id)
+            break
+
+        if(word in MACRON):
+            await context.bot.send_animation(chat_id=update.effective_chat.id, animation="CgACAgQAAxkBAAPjZgK8HwXQvB1F_XjHnuGsfGMHlvMAAmwDAALf4wRQ5tyY4x_PPR00BA", reply_to_message_id=update.message.id)
+
+
+async def apagn(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        if(update.message.sticker.file_unique_id == "AgADQgAD5ytyDg"):
+            # await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker="CAACAgEAAxkBAANPZgKb7zdtIT4QlhlazTLFUHgC6UIAAkIAA-crcg7_CaON3C-qvjQE")
+            await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker="CAACAgEAAxkBAAOJZgKo7r4mSNECAaTioqygAAFKuhM0AAJaAAPnK3IO162ahmg5xbs0BA")
+            await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker="CAACAgEAAxkBAAOTZgKpDSpkSGH49kaHGyfdHP0RQm0AAlAAA-crcg5tufLQoJOWgjQE")
+            await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker="CAACAgEAAxkBAAOeZgKpWvRp3iMXJ72JIz88H-TzJtkAAjwAA-crcg5U6vF3QF9_YjQE")
+            await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker="CAACAgEAAxkBAAOfZgKpXSmNYCpguivHpyRgptv0TcwAAjYAA-crcg7a6HRrqVAoYTQE")
+            await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker="CAACAgEAAxkBAAOgZgKpXtPhk7QatdTa06L95t-SbPYAAiYAA-crcg52mJjQ8RGlUTQE")
+            await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker="CAACAgEAAxkBAAOhZgKpYIeOkh8LjY3iquq1FEVhCsgAAkAAA-crcg5Zul2GzFC4IjQE")
+            await context.bot.send_sticker(chat_id=update.effective_chat.id, sticker="CAACAgEAAxkBAAOfZgKpXSmNYCpguivHpyRgptv0TcwAAjYAA-crcg7a6HRrqVAoYTQE")
+    except:
+        print("Cheh")
+    try:
+        print(update.message.sticker.file_unique_id)
+        print(update.message.sticker.file_id)
+    except:
+        print(update.message.animation.file_id)
+
+    
+def main():
+    app = ApplicationBuilder().token('7004038467:AAGg5mE5k47Elg_RRzgQa5FzNmkf-WibUFw').build()
+
+    # app.add_handler(CommandHandler('start', start))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
+    app.add_handler(MessageHandler(filters.Sticker.ALL | filters.ANIMATION, apagn))
+
+    app.run_polling()
+
+
+if __name__ == '__main__':
+    main()
